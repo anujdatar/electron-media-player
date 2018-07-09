e8d209fdfeb0e14cd8d930ab2fcc421828355ee7
Anuj Datar <anuj.datar@gmail.com>
2018-07-09 15:11:23 +0530

    initial commit, main window

---
+++ src/main.js
@@ -0,0 +1,37 @@
+const electron = require('electron')
+const url = require('url')
+const path = require('path')
+
+const { app, BrowserWindow, Menu } = electron
+
+let mainWindow
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    title: 'Electron Media Player',
+    icon: path.join(__dirname, './assets/images/myLogo.png')
+  })
+  
+  mainWindow.loadFile(path.join(__dirname, './src/index.html'))
+  mainWindow.on('closed', () => {
+    if (process.platform !== 'darwin'){
+      app.quit()
+    }
+  })
+}
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  if (mainWindow === null) {
+    createWindow()
+  }
+})
